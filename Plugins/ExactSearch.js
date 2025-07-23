58ea4f9693b6314c285cfa1651ed0ddd2e536012
Pretextor <87463324+Pretextor@users.noreply.github.com>
2025-07-23 10:11:30 +0700

    Create ExactSearch.js

---
+++ Plugins/ExactSearch.js
@@ -0,0 +1,9 @@
+/**
+ * @name ExactSearch
+ * @version 1.0.0
+ * @author Pretextor
+ * @authorLink https://github.com/Pretextor
+ * @description Allows user to search exact phrases.
+ * @website https://github.com/Pretextor/BetterDiscordStuff
+ * @source https://github.com/Pretextor/BetterDiscordStuff/Plugins/ExactSearch.js
+**/
